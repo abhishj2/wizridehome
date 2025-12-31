b3bc359534231f2f249f979ad83c51f7f0991217
abhishj2 <abhisakejain2@gmail.com>
2025-12-31 10:00:31 +0530

    slidefix

---
+++ download-images.bat
@@ -0,0 +1,75 @@
+@echo off
+cd /d "%~dp0src\assets\images\japan-tour"
+
+echo Downloading images...
+
+curl -L "https://images.unsplash.com/photo-1625022653882-42eede1f1c52?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aWtpZ2FpfGVufDB8fDB8fHww" -o "ikigai-concept.jpg"
+
+curl -L "https://images.pexels.com/photos/5817295/pexels-photo-5817295.jpeg?auto=compress&cs=tinysrgb&w=800" -o "day01-imperial-palace-1.jpg"
+curl -L "https://images.pexels.com/photos/5206573/pexels-photo-5206573.jpeg?auto=compress&cs=tinysrgb&w=800" -o "day01-imperial-palace-2.jpg"
+curl -L "https://images.pexels.com/photos/14892395/pexels-photo-14892395.jpeg?auto=compress&cs=tinysrgb&w=800" -o "day01-imperial-palace-3.jpg"
+curl -L "https://images.pexels.com/photos/861446/pexels-photo-861446.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-asakusa-1.jpg"
+curl -L "https://images.pexels.com/photos/13598687/pexels-photo-13598687.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-asakusa-2.jpg"
+curl -L "https://images.pexels.com/photos/13598682/pexels-photo-13598682.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-asakusa-3.jpg"
+curl -L "https://images.pexels.com/photos/10237794/pexels-photo-10237794.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-skytree-1.jpg"
+curl -L "https://images.pexels.com/photos/17399791/pexels-photo-17399791/free-photo-of-wall-of-tokyo-skytree.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-skytree-2.jpg"
+curl -L "https://images.pexels.com/photos/21759451/pexels-photo-21759451/free-photo-of-sky-tree.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-skytree-3.jpg"
+curl -L "https://images.pexels.com/photos/5905658/pexels-photo-5905658.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-shibuya-1.jpg"
+curl -L "https://images.pexels.com/photos/3402955/pexels-photo-3402955.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-shibuya-2.jpg"
+curl -L "https://images.pexels.com/photos/18848590/pexels-photo-18848590/free-photo-of-hachiko-memorial-statue-in-tokyo-japan.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day01-hachiko.jpg"
+
+curl -L "https://images.pexels.com/photos/3800108/pexels-photo-3800108.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day02-meiji-shrine-1.jpg"
+curl -L "https://lh3.googleusercontent.com/p/AF1QipOv8VA-Y43m4YnYxdvRuAnLmD_ody0e-sl395-2=s680-w680-h510" -o "day02-meiji-shrine-2.jpg"
+curl -L "https://lh3.googleusercontent.com/p/AF1QipPKWc660H5so2e8RJ9MruQmuVqdbmvD6wH9pPP5=s680-w680-h510" -o "day02-meiji-shrine-3.jpg"
+curl -L "https://images.pexels.com/photos/15829196/pexels-photo-15829196/free-photo-of-crowded-takeshita-street-harajuku-in-tokyo-japan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day02-harajuku-1.jpg"
+curl -L "https://images.unsplash.com/photo-1542062700-9b61ccbc1696?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day02-harajuku-2.jpg"
+curl -L "https://t3.ftcdn.net/jpg/06/86/34/38/240_F_686343802_QtNJY7tCzO3rxBr9rb2BzTrdnpusTmmt.jpg" -o "day02-harajuku-3.jpg"
+curl -L "https://images.unsplash.com/photo-1593073637686-cc056c151c1e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGVhbWxhYnxlbnwwfHwwfHx8MA%3D%3D" -o "day02-teamlab-1.jpg"
+curl -L "https://images.unsplash.com/photo-1593140991451-790e6b246cd5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHRlYW1sYWJ8ZW58MHx8MHx8fDA%3D" -o "day02-teamlab-2.jpg"
+curl -L "https://images.unsplash.com/photo-1555670600-170aa4c926f6?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day02-teamlab-3.jpg"
+curl -L "https://images.unsplash.com/photo-1558671434-ef7468f42a80?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day02-odaiba-1.jpg"
+curl -L "https://images.unsplash.com/958bi4L1N9s.JPG?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day02-odaiba-2.jpg"
+curl -L "https://images.unsplash.com/photo-1567597714138-3bdc30f4f493?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day02-odaiba-3.jpg"
+
+curl -L "https://images.pexels.com/photos/15829429/pexels-photo-15829429/free-photo-of-mount-fuji-in-autumn.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day03-mount-fuji-1.jpg"
+curl -L "https://images.pexels.com/photos/4077937/pexels-photo-4077937.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day03-mount-fuji-2.jpg"
+
+curl -L "https://images.pexels.com/photos/20627936/pexels-photo-20627936/free-photo-of-shinkansen-train-arriving-at-a-station.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day04-shinkansen-1.jpg"
+curl -L "https://images.unsplash.com/photo-1695833913339-f2591ac948ef?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day04-shinkansen-2.jpg"
+curl -L "https://images.unsplash.com/photo-1627052699797-b1a1b3ebb1bd?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day04-shinkansen-3.jpg"
+curl -L "https://images.unsplash.com/photo-1573700947337-61ff53eb00d5?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day04-kinkakuji-1.jpg"
+curl -L "https://lh3.googleusercontent.com/p/AF1QipOvYFbJ9JXlGH4LwCQDXvhGweo84pm83rj8NpkT=s680-w680-h510" -o "day04-kinkakuji-2.jpg"
+curl -L "https://lh3.googleusercontent.com/p/AF1QipO88HaAgI3AnuuRUDik-hR-DOUZ4VveWXx11DBv=s680-w680-h510" -o "day04-kinkakuji-3.jpg"
+curl -L "https://images.pexels.com/photos/19191148/pexels-photo-19191148/free-photo-of-outer-walls-of-nijo-castle-in-kyoto-japan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day04-nijo-castle-1.jpg"
+curl -L "https://images.unsplash.com/photo-1682849759434-1abb8720049a?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day04-nijo-castle-2.jpg"
+curl -L "https://images.unsplash.com/photo-1685845284509-2202d6e0e2eb?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day04-nijo-castle-3.jpg"
+
+curl -L "https://images.pexels.com/photos/25797659/pexels-photo-25797659/free-photo-of-kyoto-perspective.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day05-fushimi-inari-1.jpg"
+curl -L "https://images.pexels.com/photos/15857473/pexels-photo-15857473/free-photo-of-fushimi-inari-taisha-shrine-in-japan.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day05-fushimi-inari-2.jpg"
+curl -L "https://images.pexels.com/photos/15802349/pexels-photo-15802349/free-photo-of-interior-of-the-fushimi-inari-taisha-temple-in-kyoto-japan.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" -o "day05-fushimi-inari-3.jpg"
+curl -L "https://images.pexels.com/photos/19651827/pexels-photo-19651827/free-photo-of-kiyomizu-dera-temple-in-kyoto.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day05-kiyomizu-1.jpg"
+curl -L "https://images.pexels.com/photos/10889285/pexels-photo-10889285.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day05-kiyomizu-2.jpg"
+curl -L "https://images.unsplash.com/photo-1678107658651-fccc4bdae865?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTZ8fEtpeW9taXp1JTIwZGVyYXxlbnwwfHwwfHx8MA%3D%3D" -o "day05-kiyomizu-3.jpg"
+curl -L "https://images.pexels.com/photos/4323662/pexels-photo-4323662.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day05-arashiyama-1.jpg"
+curl -L "https://images.unsplash.com/photo-1598497646896-7e0d0347195b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGFyYXNoaXlhbWElMjBiYW1ib298ZW58MHx8MHx8fDA%3D" -o "day05-arashiyama-2.jpg"
+curl -L "https://images.pexels.com/photos/15829779/pexels-photo-15829779/free-photo-of-man-walking-down-the-road-in-a-bamboo-forest.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day05-arashiyama-3.jpg"
+
+curl -L "https://images.pexels.com/photos/16807973/pexels-photo-16807973/free-photo-of-photo-of-todaiji-temple-in-nara-japan.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day06-todaiji-1.jpg"
+curl -L "https://images.pexels.com/photos/1717887/pexels-photo-1717887.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day06-nara-deer-1.jpg"
+curl -L "https://images.unsplash.com/photo-1522209637010-422a41972d2c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bmFyYSUyMHBhcmt8ZW58MHx8MHx8fDA%3D" -o "day06-nara-deer-2.jpg"
+curl -L "https://images.pexels.com/photos/4058519/pexels-photo-4058519.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day06-osaka-castle-1.jpg"
+curl -L "https://images.pexels.com/photos/6226339/pexels-photo-6226339.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day06-osaka-castle-2.jpg"
+curl -L "https://images.pexels.com/photos/16961076/pexels-photo-16961076/free-photo-of-illuminated-osaka-castle-at-night.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day06-osaka-castle-3.jpg"
+curl -L "https://images.pexels.com/photos/2070026/pexels-photo-2070026.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day06-dotonbori-1.jpg"
+curl -L "https://images.pexels.com/photos/4504684/pexels-photo-4504684.jpeg?auto=compress&cs=tinysrgb&w=600" -o "day06-dotonbori-2.jpg"
+curl -L "https://images.pexels.com/photos/8050487/pexels-photo-8050487.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day06-dotonbori-3.jpg"
+
+curl -L "https://images.pexels.com/photos/18385203/pexels-photo-18385203/free-photo-of-hiroshima-peace-memorial.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" -o "day07-hiroshima-1.jpg"
+curl -L "https://images.unsplash.com/photo-1699444114670-6b36a988ec38?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day07-hiroshima-2.jpg"
+curl -L "https://images.unsplash.com/photo-1697604733264-81a69ee2315e?q=80&w=1646&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" -o "day07-hiroshima-3.jpg"
+
+echo.
+echo All images downloaded successfully!
+
+
+
